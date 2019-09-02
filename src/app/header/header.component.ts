4feaccd5d1a52a1ddce3987a070a89ef15be67fb
Will <doggy.huang@gmail.com>
2019-09-02 15:25:57 +0800

    示範 ngStyle 與 ngClass 用法

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,7 @@ export class HeaderComponent implements OnInit {
   sitename = 'Hello World';
   subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
   sitelogo = '/assets/images/logo.png';
+  counter = 0;
 
   constructor() { }
 
@@ -17,6 +18,8 @@ export class HeaderComponent implements OnInit {
   }
 
   changeTitle($event: MouseEvent) {
+    this.counter++;
+
     console.log($event);
     if ($event.ctrlKey) {
       this.sitename = 'The Will Will Web';
